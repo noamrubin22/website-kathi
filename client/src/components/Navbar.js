ddf60f4320b3a09c401b8459323bfc7818f84b58
Noam <noamru22@gmail.com>
2020-01-20 16:32:32 +0100

    on hover italic:

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,19 +4,9 @@ import "../styling.css";
 // import Projects from "./Projects";
 
 const Navbar = props => {
-  const [workState, setWorkState] = useState(props.workState);
-  console.log(props);
-  useEffect(() => {}, [workState]);
-
-  const handleClick = () => {
-    setWorkState(!workState);
-  };
-
   return (
     <div className="menu">
-      <Link onClick={handleClick} to="/">
-        work{" "}
-      </Link>
+      <Link to="/">work </Link>
       <Link to="/contact">contact</Link>
     </div>
   );
